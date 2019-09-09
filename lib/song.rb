03510684cb4a7cb90b287089debaafb18bf2d166
Levi Borland <borland.levi@gmail.com>
2019-09-09 19:27:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,40 @@ class song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0 
+  @@genres = []
+  @@artists = []
   
-  def initialize
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@genre << genre
     @@count += 1 
   end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq 
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      genre_count[genre] ||= 0 
+      genre_count[genre] += 1 
+    end
+    genre_count
+  end
+  
+  
 
 
 end
